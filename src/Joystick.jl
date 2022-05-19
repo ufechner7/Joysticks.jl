d5c56788d8b8e9962f7de3c671062f63df6ddb43
Uwe Fechner <fechner@aenarete.eu>
2022-05-20 01:30:28 +0200

    First working version

--- src/Joystick.jl
+++ src/Joystick.jl
@@ -1,5 +1,20 @@
 module Joystick
 
-# Write your package code here.
+export open_joystick, get_axis_count
+
+const JSIOCGAXES=2147576337
+
+function open_joystick(filename="/dev/input/js0")
+   file = open(filename,"r+")
+   Cint(fd(file))
+end
+
+function get_axis_count(fd::Cint)
+    axes = Ref{UInt8}()
+    if @ccall(ioctl(fd::Cint, JSIOCGAXES::Clong; axes::Ref{UInt8})::Cint) == -1
+        return -1
+    end
+    Int64(axes[])
+end
 
 end
