c9773be8c5a141c51dac16fbd41acb73f0f24fd5
Uwe Fechner <fechner@aenarete.eu>
2022-05-20 18:32:04 +0200

    Add function button_count()

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ pkg"add https://github.com/ufechner7/Joystick.jl"
 ```julia
 open_joystick(filename="/dev/input/js0")
 get_axis_count(fd::Cint)
+get_button_count(fd::Cint)
 ```
 
 ## Example
@@ -21,5 +22,6 @@ using Joystick
 
 fd = open_joystick()
 axis_count = get_axis_count(fd)
-println(axis_count)
+button_count=get_button_count(fd)
+println(axis_count, " ", button_count)
 ```
--- src/Joystick.jl
+++ src/Joystick.jl
@@ -1,8 +1,9 @@
 module Joystick
 
-export open_joystick, get_axis_count
+export open_joystick, get_axis_count, get_button_count
 
-const JSIOCGAXES=2147576337
+const JSIOCGAXES    = 2147576337
+const JSIOCGBUTTONS = 2147576338
 
 function open_joystick(filename="/dev/input/js0")
    file = open(filename,"r+")
@@ -11,10 +12,43 @@ end
 
 function get_axis_count(fd::Cint)
     axes = Ref{UInt8}()
-    if @ccall(ioctl(fd::Cint, JSIOCGAXES::Clong; axes::Ref{UInt8})::Cint) == -1
+    if @ccall(ioctl(fd::Cint, JSIOCGAXES::Culong; axes::Ref{UInt8})::Cint) == -1
         return -1
     end
     Int64(axes[])
 end
+
+function get_button_count(fd::Cint)
+    buttons = Ref{UInt8}()
+    if @ccall(ioctl(fd::Cint, JSIOCGBUTTONS::Culong; buttons::Ref{UInt8})::Cint) == -1
+        return -1
+    end
+    Int64(buttons[])
+end
+
+# /**
+#  * Returns the number of buttons on the controller or 0 if an error occurs.
+#  */
+# size_t get_button_count(int fd)
+# {
+#     __u8 buttons;
+#     if (ioctl(fd, JSIOCGBUTTONS, &buttons) == -1)
+#         return 0;
+
+#     return buttons;
+# }
+
+# int read_event(int fd, struct js_event *event)
+# {
+#     ssize_t bytes;
+
+#     bytes = read(fd, event, sizeof(*event));
+
+#     if (bytes == sizeof(*event))
+#         return 0;
+
+#     /* Error, could not read full event. */
+#     return -1;
+# }
 
 end
