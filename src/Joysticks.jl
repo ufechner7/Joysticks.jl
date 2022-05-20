24018269477fa8d0e3a3726e5436a8def63ffd38
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2022-05-20 18:40:41 -0500

    change name in copyright notice

--- src/Joysticks.jl
+++ src/Joysticks.jl
@@ -1,6 +1,6 @@
 #= MIT License
 
-Copyright (c) 2022 Uwe Fechner, Suavesito-Olimpiada
+Copyright (c) 2022 Uwe Fechner, José Joaquín Zubieta Rico
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
