a65e85309e1125f0665c3a042f5bfbe85344dacf
Uwe Fechner <fechner@aenarete.eu>
2022-05-21 12:01:39 +0200

    Improve example

--- examples/jstest.jl
+++ examples/jstest.jl
@@ -3,18 +3,28 @@ using Joysticks
 jsdevice = open_joystick()
 jsaxes   = JSAxisState()
 
-while (true)
-    event = read_event(jsdevice)
-    if ! isnothing(event) 
-        if event.type == Int(JS_EVENT_BUTTON)
-            println("Button ", event.number, " ", event.value != 0 ? "pressed" : "released")
-        elseif event.type == Int(JS_EVENT_AXIS)
-            axis = axis_state!(jsaxes, event)
-            if axis <= 6
-                println("axis: ", axis)
-                println(jsaxes)
+try
+    println("Press CTRL + C to terminate!")
+    while true
+        event = read_event(jsdevice)
+        if ! isnothing(event) 
+            if event.type == Int(JS_EVENT_BUTTON)
+                println("Button ", event.number, " ", event.value != 0 ? "pressed" : "released")
+            elseif event.type == Int(JS_EVENT_AXIS)
+                axis = axis_state!(jsaxes, event)
+                if axis <= 6
+                    println("axis: $axis, ", jsaxes)
+                end
             end
         end
+        sleep(0.001)
     end
-    sleep(0.001)
+catch e
+    if typeof(e) <: InterruptException
+        println("Terminated program!")
+    else
+        rethrow(e)
+    end
 end
+
+
