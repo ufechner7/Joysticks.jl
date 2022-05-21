11026dde63234e27d09a2f41986981b4bf85013a
Uwe Fechner <fechner@aenarete.eu>
2022-05-21 11:28:29 +0200

    Improve tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,11 @@ using Joysticks
 using Test
 
 @testset "Joysticks.jl" begin
-    jld = open_joystick()
-    axis_count = jld.axis_count
-    @test axis_count > 0
+    js = open_joystick()
+    if ! isnothing(js)
+        @test js.axis_count > 0
+        @test js.button_count > 0
+        event = read_event(js)
+        @test event.type & Int(JS_EVENT_INIT) == Int(JS_EVENT_INIT)
+    end
 end
