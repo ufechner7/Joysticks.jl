62a178ec2b5f09991e0e3478991596e6fc6a1f12
Uwe Fechner <fechner@aenarete.eu>
2022-06-11 22:08:36 +0200

    Add more buttons

--- examples/read_jsbuttons.jl
+++ examples/read_jsbuttons.jl
@@ -11,5 +11,17 @@ end
 on(jsbuttons.btn2) do val
     if val println("Button 2 pressed!") end
 end
+on(jsbuttons.btn3) do val
+    if val println("Button 3 pressed!") end
+end
+on(jsbuttons.btn4) do val
+    if val println("Button 4 pressed!") end
+end
+on(jsbuttons.btn5) do val
+    if val println("Button 5 pressed!") end
+end
+on(jsbuttons.btn6) do val
+    if val println("Button 6 pressed!") end
+end
 
 nothing
